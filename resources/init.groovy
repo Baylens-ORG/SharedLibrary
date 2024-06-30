a37100d448d84c915ff753628c53a925bd805c83
Baylen Doss <baylendoss12@gmail.com>
2024-06-30 09:31:17 -0700

    test

--- resources/init.groovy
+++ resources/init.groovy
@@ -1,4 +1,6 @@
 def initializeLibrary() {
    println("HELLOOOOOOOOOOOOOOOOOOOO")
     // Add any additional initialization logic here
+    throw new IllegalArgumentException("Input cannot be empty") // Throw an exception
+
 }
